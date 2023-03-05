2a84ae8c27c2e72d41f1c67f91a10c863a365d19
anu bazarragchaa <38427020+anu-bazar@users.noreply.github.com>
2023-03-05 15:00:46 +0100

    Update Assignment1.R

--- Assignment1.R
+++ Assignment1.R
@@ -1,61 +1,42 @@
-#TASK 1 
-
+#TASK 1 STARTS HERE
 
 library (dplyr)
 library (ggplot2)
-#UPLOAD DATA
-df <- BM_1A_AFODDC_Task1
-box_plot <- boxplot(Mushroom_yield ~ Mushroom_strain, data = df,
+
+datamushroom <- BM_1A_AFODDC_Task1
+box_plot <- boxplot(Mushroom_yield ~ Mushroom_strain, data = datamushroom,
                     varwidth = TRUE, log = "y", las = 1)
-model<-lm(Mushroom_yield ~ Mushroom_strain, data = df)
-shapiro.test(model$residuals)
 #posthoc test
-anovasummary <- aov(Mushroom_yield ~ Mushroom_strain, data = df)
+anovasummary <- aov(Mushroom_yield ~ Mushroom_strain, data = datamushroom)
 summary(anovasummary)
 tukey.anovasummary<-TukeyHSD(anovasummary)
 tukey.anovasummary
 
 
-#TASK 2 
-
+#TASK 2 STARTS HERE
 library (dplyr)
 library (ggplot2)
 df <- BM_1A_AFODDC_Task2
-
-#Analysis
-
 model<-lm(Stature~Femur, data = df)
 summary(model)
-
-#Plot
+summary(model)$r.squared
 plot(df$Stature, df$Femur, xlab="Stature", ylab="Femur")
-abline(lm(Stature~Femur, data = df), col = "blue")
+plot(df$Stature, df$Femur, xlab="Stature", ylab="Femur", 
+     xlim = c(165,max(df$Stature, na.rm=TRUE)),
+     ylim = c(392,max(df$Femur, na.rm=TRUE)))
+abline(coef = c(0,2.34
+))
 
-
-#Residual analysis
-#Linearity and homoscedasticity
-plot(model, pch=16, col="blue",lwd=2, which=1)
-plot(model, pch=16, col="blue",lwd=2, which=3)
-#Normality
-plot(model, pch=16, col="blue",lwd=2, which=2)
-shapiro.test(model$residuals)
-
-#TASK 3
+#TASK 3 STARTS HERE
 
 library(dplyr)
 library(rstatix)
-
-#Uploading data
 a <- c(176.5,174.6,179.3,173.1,175.3,180.3,175.6,173.4,176.7,174)
 b <- c(181.2,180,183.6,183.2,184,180.3,182.6,183.2,182.5,183)
 C <- list(a,b)
 names(C) <- c(paste("Group 1\n n=" , length(a) , sep=""), paste("Group 2\n n=" , length(b) , sep=""))
-
-#Set graphical parameters
 par(mgp=c(3,2,0))
+boxplot(C , col="#FFFF00" , ylab="value" )
+dataset3 <- as.numeric(BM_1A_AFODDC_Task3[-c(1),])
 
-#Boxplot
-boxplot(C , col="#808080" , ylab="value" )
-
-#Test statistic Mann-Whitney test
-wilcox.test(a , b, paired=F, exact=F)
+wilcox.test(a , b, paired=T, exact=F)
